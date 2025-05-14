a2c529ee31b026187ba9f8fae9d889c86ed395bc
CoEdd <141335632+CoEdd@users.noreply.github.com>
2025-05-14 17:05:01 +0800

    Refactor app.py to use environment variable for port configuration

--- mediscan-ai/app/app.py
+++ mediscan-ai/app/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from routes import routes
 
@@ -5,4 +6,6 @@ app = Flask(__name__)
 app.register_blueprint(routes)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    # Ensure you use the port from the environment variable (Render will set this)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
